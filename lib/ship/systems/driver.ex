fc48bf023996f4a322b1d7e3cfae9fd9c92392da
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-12-22 14:50:06 -0800

    update component positions

--- lib/ship/systems/driver.ex
+++ lib/ship/systems/driver.ex
@@ -4,9 +4,24 @@ defmodule Ship.Systems.Driver do
   """
   @behaviour ECSx.System
 
+  alias Ship.Components.XPosition
+  alias Ship.Components.YPosition
+  alias Ship.Components.XVelocity
+  alias Ship.Components.YVelocity
+
   @impl ECSx.System
   def run do
-    # System logic
-    :ok
+    for {entity, x_velocity} <- XVelocity.get_all() do
+      x_position = XPosition.get(entity)
+      new_x_position = x_position + x_velocity
+      XPosition.update(entity, new_x_position)
+    end
+
+    # Once the x-values are updated, do the same for the y-values
+    for {entity, y_velocity} <- YVelocity.get_all() do
+      y_position = YPosition.get(entity)
+      new_y_position = y_position + y_velocity
+      YPosition.update(entity, new_y_position)
+    end
   end
 end
