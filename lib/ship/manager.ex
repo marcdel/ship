acffe0ba1faf23542e3207fc2287311e1793f5c8
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-12-28 12:31:01 -0800

    initialize ships

--- lib/ship/manager.ex
+++ lib/ship/manager.ex
@@ -13,6 +13,23 @@ defmodule Ship.Manager do
   def startup do
     # Load ephemeral components during first server start and again
     # on every subsequent app restart
+    for _ships <- 1..40 do
+      # First generate a unique ID to represent the new entity
+      entity = Ecto.UUID.generate()
+
+      # Then use that ID to create the components which make up a ship
+      Ship.Components.ArmorRating.add(entity, 0)
+      Ship.Components.AttackDamage.add(entity, 5)
+      Ship.Components.AttackRange.add(entity, 10)
+      Ship.Components.AttackSpeed.add(entity, 1.05)
+      Ship.Components.HullPoints.add(entity, 50)
+      Ship.Components.SeekingTarget.add(entity)
+      Ship.Components.XPosition.add(entity, Enum.random(1..100))
+      Ship.Components.YPosition.add(entity, Enum.random(1..100))
+      Ship.Components.XVelocity.add(entity, 0)
+      Ship.Components.YVelocity.add(entity, 0)
+    end
+
     :ok
   end
 
